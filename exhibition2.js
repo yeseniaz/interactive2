f90f6a5baceea471ab7055c1d78fe54c7e7c8373
yesenia <yeseniazamora@cca.edu>
2019-04-30 16:26:39 -0700

    aydyi

--- exhibition2.js
+++ exhibition2.js
@@ -1,5 +1,3 @@
-<script type="text/javascript" src="Event.js"></script>
-<script type="text/javascript" src="Magnifier.js"></script>
 <script type="text/javascript">
 var evt = new Event(),
     m = new Magnifier(evt);
