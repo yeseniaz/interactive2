ba9d1f7d49d1db2a462acdde6b66210f3466623d
yesenia <yeseniazamora@cca.edu>
2019-04-30 17:05:34 -0700

    hi

--- exhibition2.js
+++ exhibition2.js
@@ -1,4 +1,79 @@
-<script type="text/javascript">
-var evt = new Event(),
-    m = new Magnifier(evt);
+function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+
+  img.parentElement.insertBefore(glass, img);
+
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  function moveMagnifier(e) {
+    var pos, x, y;
+    e.preventDefault();
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /* Prevent the magnifier glass from being positioned outside the image: */
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    /* Set the position of the magnifier glass: */
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /* Display what the magnifier glass "sees": */
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /* Get the x and y positions of the image: */
+    a = img.getBoundingClientRect();
+    /* Calculate the cursor's x and y coordinates, relative to the image: */
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /* Consider any page scrolling: */
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
+
+<script>
+/* Execute the magnify function: */
+magnify("myimage", 3);
+/* Specify the id of the image, and the strength of the magnifier glass: */
 </script>
+
+
+window.addEventListener('resize',pageResized)
+
+
+function pageResized(){
+  console.log(window.innerHeight, window.innerWidth)
+
+//   uncomment this to see the font size resize based on how tall the window is
+
+//   let windowPercentOfScreen = (1 - (window.innerHeight / window.screen.height)) * 100
+
+//   let colorFromScreenHeight = 'hsl(0,0%,' + windowPercentOfScreen + '%)'
+//   console.log(colorFromScreenHeight)
+//   document.body.style.color = colorFromScreenHeight
+
+
+}
+
+pageResized()
